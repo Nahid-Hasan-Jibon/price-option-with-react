4ed556c15f172f24fdae54653ff5587383fef8eb
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2025-01-19 22:09:18 +0600

    toogle btn added

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
 import { list } from "postcss";
-import React from "react";
+import React, {useState } from "react";
 import Link from "../Components/link/Link";
+import { RiMenu2Fill } from "react-icons/ri";
+import { IoIosClose } from "react-icons/io";
 
 const NavBar = () => {
+    const [open , setOpen] =useState(false)
   const routes = [
     { id: 1, name: "Home", path: "/" },
     { id: 2, name: "About", path: "/about" },
@@ -14,6 +17,12 @@ const NavBar = () => {
   return (
 
     <nav>
+        <div className="text-3xl md:hidden" onClick={()=>setOpen(!open)}>
+            {
+                open===true?<IoIosClose />:<RiMenu2Fill/>
+            }
+        
+        </div>
         <ul className="md:flex ">
         {
             routes.map(route=><Link key={route.id }route={route}></Link> )
