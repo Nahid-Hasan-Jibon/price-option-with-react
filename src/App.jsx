cdf9f0f9d8ae9d6f4bbe1fefb2ffcd7892341b67
Nahid-Hasan-Jibon <smnahidhasanjibon@gmail.com>
2025-01-20 05:40:49 +0600

    chart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import DaisyNav from './Components/DaisyNav/DaisyNav'
+import LineChart from './Components/LineChart/LineChart'
 import PriceOptions from './Components/PriceOptions/PriceOptions'
 import NavBar from './NavBar/NavBar'
 
@@ -13,6 +14,7 @@ function App() {
      <NavBar></NavBar>
       <h1>Vite + React</h1>
       <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
       
       
     </>
